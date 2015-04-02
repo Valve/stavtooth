f0451155704191fa897d16a51ac65922d5e2a39f
Valentin Vasilyev <valentin.vasilyev@outlook.com>
2015-04-02 19:25:32 +0300

    Patient edit + print improvements

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -3,3 +3,6 @@ $("#diagnosis-template").on("change", function(e){
   var desc = $(this).val();
   $("#patient_diagnosis, #visit_diagnosis").val(desc);
 });
+$("#print-patient").on("click", function(){
+  window.print();
+});
